1a3f57834f4defac68f7bd5cc4cacee207e820df
Juan M Torres <juanmatorres2@yahoo.com>
2021-05-24 18:11:50 -0300

    Arreglo pagina

--- js/javascript.js
+++ js/javascript.js
@@ -24,13 +24,7 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
 
-    
-//Para mostrar el menu
-document.querySelector(".showmenu").addEventListener("click", mostrar);
-
-function mostrar(){
-    document.querySelector(".navegador").classList.toggle("show");
-}
 
 })
+
 
---
+++ js/menuResponsive.js
@@ -0,0 +1,11 @@
+
+ document.addEventListener("DOMContentLoaded", function(){
+       
+       //Para mostrar el menu
+        document.querySelector(".showmenu").addEventListener("click", mostrar);
+
+        function mostrar(){
+            document.querySelector(".navegador").classList.toggle("show");
+        }
+})
+    
